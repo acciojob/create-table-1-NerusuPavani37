f501c832f3582d907e05db80d488b8aca51bae74
Pavani <136723974+NerusuPavani37@users.noreply.github.com>
2025-05-15 20:58:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function insert_Row() {
-    //Write your code here
-  
-  
+
+  function insert_Row() {
+    let parent = document.getElementById("sampleTable");
+    let newRow=parent.insertRow(0);
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+    cell1.innerHTML = "New Cell1"
+    cell2.innerHTML = "New Cell2"
 }
